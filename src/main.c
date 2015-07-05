a5f6e04b7ae572ed8ec402b7ea2d771cb249e287
Loïc Pirez <loic.pirez@epitech.eu>
2015-07-05 21:32:40 +0200

    Showing text on debug console

--- src/main.c
+++ src/main.c
@@ -3,5 +3,8 @@
 KOS_INIT_FLAGS(INIT_DEFAULT | INIT_MALLOCSTATS);
 
 int main(int argc, char **argv) {
+  puts("\n\nThis text will be on the console debug.\n");
+  puts("Here, we got a black screen on the 'main' screen.\n");
+  while (1);
   return 0;
 }
